c22582c968c5e7a96e872bffe7dbc11bae33954f
JR Newman 01 <jrosales.newman.2025.1@gmail.com>
2025-03-06 19:00:43 -0500

    add: se agrega divicion

--- calcular.py
+++ calcular.py
@@ -4,7 +4,12 @@ def suma(a, b):
 def resta(a, b):
     return a - b
 
+def division(a, b):
+    if b == 0:
+        raise ValueError("No se puede dividir por cero")
+    return a / b
 
 if __name__ == "__main__":
     print("Suma de 5 + 3 =", suma(5, 3))
     print("Resta de 5 - 3 =", resta(5, 3))
+    print("División de 6 / 2 =", division(6, 2))
